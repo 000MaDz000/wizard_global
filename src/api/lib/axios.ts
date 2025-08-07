0a0051cb5354ed358abb76a73d1750baf36120a0
MaDz <000madz000@gmail.com>
2025-08-07 20:57:30 +0300

    resolve the environment variables read

--- src/api/lib/axios.ts
+++ src/api/lib/axios.ts
@@ -1,6 +1,6 @@
 import axiosBase from "axios";
 
-const token = (window as any).env.STRAPI_TOKEN;
+const token = (import.meta as any).env.VITE_STRAPI_TOKEN;
 
 export const axios = axiosBase.create({
     headers: {
@@ -9,6 +9,6 @@ export const axios = axiosBase.create({
 });
 
 // Base URL - you should set this to your Strapi instance URL
-export const STRAPI_BASE_URL = (window as any).env.PUBLIC_STRAPI_URL || 'http://localhost:1337';
+export const STRAPI_BASE_URL = (import.meta as any).env.VITE_PUBLIC_STRAPI_URL || 'http://localhost:1337';
 
 axios.defaults.baseURL = `${STRAPI_BASE_URL}/api`;
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,9 @@
 import { defineConfig } from 'vite'
-import { parse } from 'dotenv';
-import { readFileSync } from "fs";
 import react from '@vitejs/plugin-react';
-import { join } from 'path';
 
-const envPath = join(__dirname, ".env");
-let env = {};
-
-try {
-    env = parse(
-        readFileSync(envPath).toString()
-    );
-}
-catch {
-    env = process.env;
-}
 
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    define: {
-        "env": env
-    }
 })
