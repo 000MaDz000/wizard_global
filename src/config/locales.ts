33dfad181267e51b7a1fc6731d0117da633dcf8d
amrshanab <hmnm5485@gmail.com>
2025-09-04 18:30:58 +0300

    la

--- src/config/locales.ts
+++ src/config/locales.ts
@@ -1,27 +1,31 @@
+import arab from '../assets/saudi-arabia.png'
+import english from '../assets/united-kingdom_555417.png'
+import china from '../assets/china.png'
+import malay from '../assets/malaysia_5327355.png'
 export const Locales = [
     {
         code: "ar",
         name: "ع",
-        flag: "/public/saudi-arabia.png",
+        flag: arab,
         dir: "rtl"
     },
     {
         code: "en",
         name: "EN",
-        flag: "/public/united-kingdom_555417.png",
+        flag: english,
     
         dir: "ltr"
     },
     {
         code: "zh",
         name: "中文",
-        flag: "/public/china.png",
+        flag: china,
         dir: "ltr"
     },
     {
         code: "ms",
         name: "MS",
-        flag: "/public/malaysia_5327355.png",
+        flag: malay,
         dir: "ltr"
     }
 ];
